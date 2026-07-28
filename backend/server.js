09c9cef6a244426c9fc0458a03f4055f1df0138e
fiekryadiperdana14-star <fiekryadiperdana14-star@users.noreply.github.com>
2026-07-28 07:23:14 +0700

    Hardcode production database env to fix Vercel login

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
+require('dotenv').config({ path: '.env.prod' });
 require('dotenv').config();
 
 const app = express();
